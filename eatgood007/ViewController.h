566a10fd446cd4819fe4c7c66223ce9ce5cfa39e
lalala34 <mevee90@yeah.net>
2014-09-20 18:20:27 +0800

    Initial Commit

---
+++ eatgood007/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eatgood007
+//
+//  Created by 张 伟 on 14-9-20.
+//  Copyright (c) 2014年 eatgood007. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ eatgood007/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  eatgood007
+//
+//  Created by 张 伟 on 14-9-20.
+//  Copyright (c) 2014年 eatgood007. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
